b603563584fc1e85eb06b7038454ad8edbd91141
Felipe Pedro <felipepedro@aluno.senai.br>
2024-02-21 09:47:38 -0300

    Adiciona formulário para adicionar tarefas

--- App.jsx
+++ App.jsx
@@ -2,18 +2,51 @@ import { Text, View, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const [task, setTask] = useState("");
+
 	return (
-		<View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
-    </View>
+<View style={styles.container}>
+  <View style={styles.form}>
+    <TextInput
+    style={styles.inputline}
+      placeholder="Digite uma nova tarefa"
+      onChangeText={setTask}
+      value={task}
+    />
+    <TouchableOpacity
+      style={styles.button}
+    >
+      <Text style={styles.buttontext} >Adicionar</Text>
+    </TouchableOpacity>
+  </View>
+  <StatusBar style="auto" />
+</View>
 	);
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
   },
-});
+  form: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  inputline: {
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    width: "75%",
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttontext: {
+    textAlign: "center",
+    color: "white",
+  },
+  }
+);
